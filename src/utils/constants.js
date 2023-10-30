36cc413f5561d85b10c6d440f95fc05ec89f6b35
tushar885 <tushar8104@gmail.com>
2023-10-30 23:54:47 +0530

    routes set to production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,31 +1,37 @@
 // For production
-// export const SIGN_IN_URL = "https://artisan-api-q9uw.onrender.com/auth/signin";
-// export const SIGN_UP_URL = "https://artisan-api-q9uw.onrender.com/auth/signup";
-// export const CREATE_USER =
-//   "https://artisan-api-q9uw.onrender.com/create-userProfile";
-// export const GET_ALL_USERS =
-//   "https://artisan-api-q9uw.onrender.com/getAllUsers";
-// export const UPLOAD_TEMPLATE =
-//   "https://artisan-api-q9uw.onrender.com/addNewTemplate";
-// export const GET_ALL_TEMPLATES =
-//   "https://artisan-api-q9uw.onrender.com/getAllTemplates";
-// export const BASE_URL = "https://artisan-api-q9uw.onrender.com/";
-// export const LOADER_GIF_URL =
-//   "https://cdn.dribbble.com/users/935167/screenshots/2896660/media/2d8f0934f7ff1c042c3a8dddc08d4364.gif";
-// export const GET_KHOJOPROFILE_BY_ID = "https://artisan-api-q9uw.onrender.com/getKhojoProfileByID/"
-
-// For local Development
-export const SIGN_IN_URL = "http://localhost:3000/auth/signin";
-export const SIGN_UP_URL = "http://localhost:3000/auth/signup";
-export const CREATE_USER = "http://localhost:3000/create-userProfile";
-export const GET_ALL_USERS = "http://localhost:3000/getAllUsers";
-export const UPLOAD_TEMPLATE = "http://localhost:3000/addNewTemplate";
-export const GET_ALL_TEMPLATES = "http://localhost:3000/getAllTemplates";
-export const BASE_URL = "http://localhost:3000/";
+export const SIGN_IN_URL = "https://artisan-api-q9uw.onrender.com/auth/signin";
+export const SIGN_UP_URL = "https://artisan-api-q9uw.onrender.com/auth/signup";
+export const CREATE_USER =
+  "https://artisan-api-q9uw.onrender.com/create-userProfile";
+export const GET_ALL_USERS =
+  "https://artisan-api-q9uw.onrender.com/getAllUsers";
+export const UPLOAD_TEMPLATE =
+  "https://artisan-api-q9uw.onrender.com/addNewTemplate";
+export const GET_ALL_TEMPLATES =
+  "https://artisan-api-q9uw.onrender.com/getAllTemplates";
+export const BASE_URL = "https://artisan-api-q9uw.onrender.com/";
 export const LOADER_GIF_URL =
   "https://cdn.dribbble.com/users/935167/screenshots/2896660/media/2d8f0934f7ff1c042c3a8dddc08d4364.gif";
 export const GET_KHOJOPROFILE_BY_ID =
-  "http://localhost:3000/getKhojoProfileByID/";
+  "https://artisan-api-q9uw.onrender.com/getKhojoProfileByID/";
+export const GET_FILTERED_PROFILES =
+  "https://artisan-api-q9uw.onrender.com/getKhojoProfilesByFilters";
+
+// For local Development
+// export const SIGN_IN_URL = "http://localhost:3000/auth/signin";
+// export const SIGN_UP_URL = "http://localhost:3000/auth/signup";
+// export const CREATE_USER = "http://localhost:3000/create-userProfile";
+// export const GET_ALL_USERS = "http://localhost:3000/getAllUsers";
+// export const UPLOAD_TEMPLATE = "http://localhost:3000/addNewTemplate";
+// export const GET_ALL_TEMPLATES = "http://localhost:3000/getAllTemplates";
+// export const BASE_URL = "http://localhost:3000/";
+// export const LOADER_GIF_URL =
+//   "https://cdn.dribbble.com/users/935167/screenshots/2896660/media/2d8f0934f7ff1c042c3a8dddc08d4364.gif";
+// export const GET_KHOJOPROFILE_BY_ID =
+//   "http://localhost:3000/getKhojoProfileByID/";
+
+// export const GET_FILTERED_PROFILES =
+//   "http://localhost:3000/getKhojoProfilesByFilters";
 
 export const DISTRICTS = [
   "Ajmer",
